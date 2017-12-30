f7c338044b9fedac1da69acbb1078300d282c8b7
Vojta Drabek <vojta.drabek@gmail.com>
2017-12-30 16:36:51 +0100

    Initial commit

---
+++ app/src/main/java/com/draabek/androsigner/MainActivity.java
@@ -0,0 +1,59 @@
+package com.draabek.androsigner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView tv = findViewById(R.id.content);
+        setSupportActionBar(toolbar);
+        Web3j web3 = Web3jFactory.build(new HttpService( "https://ropsten.infura.io/tmbhNp6pHaBMdPKYsP7A"));
+        Web3ClientVersion web3ClientVersion = null;
+        try {
+            web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+        tv.setText(clientVersion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
